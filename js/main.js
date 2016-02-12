7c5c060dcff3133533cb741e603fa059ec94d9e8
revalidate <reva.bhatt@gmail.com>
2016-02-11 22:30:08 -0800

    added about me, skills, and contact section

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){       
+   var scroll_start = 0;
+   var startchange = $('.nav');
+   var offset = startchange.offset();
+   $(document).scroll(function() { 
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset.top) {
+          $('.nav').css('background-color', '#f2dede');
+       } else {
+          $('.nav').css('background-color', 'transparent');
+       }
+   });
+
+});
