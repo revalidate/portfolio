c268e5663563e0f6db88e1c44910f9a044ff7afa
revalidate <reva.bhatt@gmail.com>
2016-03-09 13:50:53 -0800

    changed colors around a bit

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
-$(document).ready(function(){       
+$(document).ready(function(){
    var scroll_start = 0;
    var startchange = $('.nav');
    var offset = startchange.offset();
-   $(document).scroll(function() { 
+   $(document).scroll(function() {
       scroll_start = $(this).scrollTop();
       if(scroll_start > offset.top) {
-          $('.nav').css('background-color', '#EDCAC6');
+          $('.nav').css('background-color', '#ECE350');
        } else {
           $('.nav').css('background-color', 'transparent');
        }
    });
 
-});
+   $('.header-title').hover(function() {
+     $(this).addClass('animated pulse');
+   });
 
+});
